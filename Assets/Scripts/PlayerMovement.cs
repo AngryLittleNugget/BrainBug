650d42108018da264799566702275d66eebed594
AngryLittleNugget <keenanmwalter@gmail.com>
2025-08-13 14:54:17 -0600

    Commit 1 8-13-25
    Basic Background Implemented, little dude moves around

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float speed;
+    public float speed = 2;
+    Vector2 movement;
+    Rigidbody2D rb;
+    GameObject player;
+    float movementHorizontal;
+    float movementVertical;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        player = GetComponent<GameObject>();
     }
-//My name is *wiki wiki* slim shady
+
     // Update is called once per frame
     void Update()
     {
-        
+        movementHorizontal = Input.GetAxisRaw("Horizontal");
+        movementVertical = Input.GetAxisRaw("Vertical");
+
+        rb.linearVelocityX = movementHorizontal * speed;
+        rb.linearVelocityY = movementVertical * speed;
+
+        if (movementHorizontal != 0 || movementVertical != 0)
+        {
+            DisplayAxis();
+        }
+
+        /* if (Input.GetKey(KeyCode.LeftArrow))
+         {
+             rb.linearVelocityX = -speed;
+         }
+          if (Input.GetKey(KeyCode.RightArrow))
+         {
+             rb.linearVelocityX = speed;
+         }
+          if (Input.GetKey(KeyCode.UpArrow))
+         {
+             rb.linearVelocityY = speed;
+         }
+          if (Input.GetKey(KeyCode.DownArrow))
+         {
+             rb.linearVelocityY = -speed;
+         }*/
+
+    }
+
+    void DisplayAxis()
+    {
+        Debug.Log($" Horizontal axis: {movementHorizontal} Vertical Axis: {movementVertical}");
     }
 }

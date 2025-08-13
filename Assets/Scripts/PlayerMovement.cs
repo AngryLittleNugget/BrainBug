f9d0f8da131c0d042a3b40debc9632d869b7c7ab
AngryLittleNugget <keenanmwalter@gmail.com>
2025-08-13 13:29:11 -0600

    Test Push
    After a rough start, making sure pushes are actually doing anything.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
+//My name is *wiki wiki* slim shady
     // Update is called once per frame
     void Update()
     {
